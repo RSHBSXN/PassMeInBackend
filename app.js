001e420affd58f97c618e97ef295fab2e6958b33
rshbb <rfactor78@gmail.com>
2019-04-30 11:40:11 +0530

    added login

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
+
+//Database connection and importing both models
+
+const port = process.env.PORT;
+require("./database/connectMongoose");
+const User = require("./database/Schema/userSchema");
+const cors =  require("cors");
+
+//const Card = require("./database/Schema/cardSchema");
+
+//include express for routing purposes
 const express = require("express");
-const server = express();
-//Routes
-server.get("/",(req,res)=>{
-    res.send("CONNECTED");
-})
-
-
-server.listen(200,console.log("Running Server"));
+const app = express();
+app.use(cors({credentials:true,origin:true}))
+app.set("view engine","pug");
+app.set("views","./views");
+app.use(express.urlencoded());
+app.use(express.json());
+async function disp(){
+    console.log(await User.find());
+}
+async function findByEm(em){
+    return await User.findOne({email:em});
+}
+async function findByNum(num){
+    return await User.findOne({number:num});
+}
+//Finally include authy for push notification.
+const authy = require("authy")(process.env.APIKEY);
+//Routes Here
+app.post("/login",async (req,res)=>{
+    
+    let number = req.body.phone;
+    // let email = req.body['email'];
+    let result = {};
+    result = await findByNum(number);
+    console.log(result)
+    if(!result)
+        res.send({status:"user not found"});
+    else{
+        authy.send_approval_request(result.authId,{message:"click on approve to login"},"","",async(err,resp)=>{
+            if(err)
+                res.status(404).send({"success":false,"message":"Kuch gdbd hai"})
+            else{
+                let uuid = resp.approval_request.uuid;
+                await setTimeout(()=>{
+                    authy.check_approval_status(uuid,(err,resp)=>{
+                    if(err)
+                        res.status(404).send({"success":false,"message":"Kuch gdbd hai"})
+                    else
+                        res.status(200).send({"success":true,"message":"Kuch gdbd hai","data":resp.approval_request.status})
+                    })
+                },30000);
+            }
+        });//ek bar await hata ke dkehte h
+    }
+});
+//port listening here
+app.listen(port,console.log("listening at port",port));
