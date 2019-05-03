4d1b7b3f785183a79de94e7496f8f5fb69d10940
rshbb <rfactor78@gmail.com>
2019-05-03 22:30:00 +0530

    Added Card Schema

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 //Database connection and importing both models
 require("./database/connectMongoose");
 const User = require("./database/Schema/userSchema");
-//const Card = require("./database/Schema/cardSchema");
+const Card = require("./database/Schema/cardSchema");
 
 //include express for routing purposes
 const express = require("express");
@@ -12,73 +12,64 @@ const app = express();
 app.use(cors({credentials:true,origin:true}))
 app.set("view engine","pug");
 app.set("views","./views");
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-async function disp(){
-    console.log(await User.find());
+async function registerUser(req,res){
+    const email = req.body["email"];
+    const phone = req.body["phone"];
+    let resp = responseCode;
+    if(!await User.findByEmail(email) && !await User.findByNumber(phone)){
+        authy.register_user(email,phone,process.env.COUNTRY_CODE,true,async(err,resp)=>{
+            if(err){
+                resp.message = err;
+                res.send({status:false,message:err});
+            }
+            else{
+                const result = await User.createUser(email,phone,resp.user.id);
+                res.send({status:true,message:result});
+            }
+        });
+    }
+    else{
+        res.send({status:false,message:'user already exists'});
+    }
 }
-async function findByEm(em){
-    return await User.findOne({email:em});
-}
-async function findByNum(num){
-    return await User.findOne({number:num});
-}
-async function createUser(email,number,authId){
-    const user = new User({
-        email:email,
-        number:number,
-        authId:authId
-    });
-    await user.save();
-}
-//Finally include authy for push notification.
-const authy = require("authy")(process.env.API_KEY);
-
-//Routes Here
-app.post("/login",async (req,res)=>{
+async function loginUser(req,res){
     let number = req.body["phone"];
     let email = req.body['email'];
     let result = {};
     console.log(number,email)
     if(number != null){
-        result = await findByNum(number);
+        result = await User.findByNumber(number)
     }
     else if(email != null){
-        result = await findByEm(email);
+        result = await User.findByEmail(email)
     }
-    console.log(result)
+    console.log(result);
     if(!result)
-        res.send({status:"user not found"});
+        res.send({status:false,message:"User not found"});
     else{
-        authy.send_approval_request(result.authId,{message:"click on approve to login"},"","",async(err,resp)=>{
+        authy.send_approval_request(result.authId,{message:"click on approve to login"},"","",(err,resp)=>{
             if(err)
-                res.status(404).send(err)
+                res.status(500).send(err)
             else{
                 let uuid = resp.approval_request.uuid;
-                await setTimeout(()=>{
+                setTimeout(()=>{
                     authy.check_approval_status(uuid,(err,resp)=>{
                     if(err)
-                        res.status(404).send({"success":false,"message":"Kuch gdbd hai"})
+                        res.status(500).send({status:false,message:"Kuch gdbd hai"})
                     else
-                        res.status(200).send({"success":true,"message":"Kuch gdbd hai","data":resp.approval_request.status})
+                        res.status(200).send({status:true,message:resp.approval_request.status})
                     })
                 },30000);
             }
-        });//ek bar await hata ke dkehte h
+        });
     }
-});
-app.post("/register",async (req,res)=>{
-    const email = req.body["email"];
-    const phone = req.body["phone"];
-    authy.register_user(email,phone,"+91",true,(err,resp)=>{
-        if(err)
-            res.send(err);
-        else{
-            createUser(email,phone,resp.user.id);
-            disp();
-            res.send(resp);
-        }
-    });
-});
+}
+//Finally include authy for push notification.
+const authy = require("authy")(process.env.API_KEY);
+//Routes Here
+app.post("/login",loginUser);
+app.post("/register",registerUser);
 
 app.listen(port,console.log("listening at port:",port));
--- database/Schema/cardSchema.js
+++ database/Schema/cardSchema.js
@@ -1,8 +1,59 @@
 const mongoose = require("mongoose");
-let userSchema = new mongoose.Schema({
+let cardSchema = new mongoose.Schema({
+    uid:String,
+    url:String,
     email:String,
-    number:String,
-    authId:String
+    password:String,
+    domainName:String
 });
-const User = mongoose.model("User",userSchema);
-module.exports = User;
+class Card{
+    //CRUD ON CARDS
+    async addCard(uid,url,email,pass,dom){
+        let card = new Card({
+            uid:uid,
+            url:url,
+            email:email,
+            password:pass,
+            domainName:dom
+        });
+        try{
+            const result = await card.save();
+            return result;
+        }
+        catch(e){
+            return e;
+        }
+    }
+    async findAllCards(uid){
+        try{
+            const result = await this.model.find({uid:uid});
+            return result;
+        }
+        catch(e){
+            return e;
+        }
+    }
+    async updateCard(id,password){
+        try{
+            const result = await this.model.findByIdAndUpdate(id,{
+                $set:{
+                    password:password
+                }
+            },{new:true});
+            return result;
+        }
+        catch(e){
+            return e;
+        }
+    }
+    async deleteCard(id){
+        try{
+            const result = await this.model.findByIdAndRemove(id);
+            return result;
+        }
+        catch(e){
+            return e;
+        }
+    }
+}
+Card.model = new mongoose.model("Card",cardSchema);
